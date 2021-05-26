1915e32c738ca52af8ac85ea873f004c2eecef58
talthur <talthur.ga@gmail.com>
2021-05-26 18:58:04 -0300

    novos metodos de aumentar array e adicionar em qualquer posicao

--- src/br/com/talthur/estruturadadosvetor/Vetor.java
+++ src/br/com/talthur/estruturadadosvetor/Vetor.java
@@ -1,69 +1,92 @@
 package br.com.talthur.estruturadadosvetor;
 
 public class Vetor {
-	
+
 	private String[] elementos;
 	private int tamanhoRealArray;
-	
+
 	public Vetor(int capacidadeVetor) {
 		this.elementos = new String[capacidadeVetor];
 		this.tamanhoRealArray = 0;
 	}
-	
-	public boolean adiciona(String elemento) {
+
+	public boolean adicionQualquerLugar(int index, String elemento) {
 		
-		if(tamanhoRealArray < this.elementos.length) {
-			this.elementos[this.tamanhoRealArray] = elemento;
-			this.tamanhoRealArray ++;
+		if (elementos[index] == null) {
+			elementos[index] = elemento;
 			return true;
-		} return false;
+		} else {
+			
+			if(this.elementos.length > this.tamanhoRealArray) {
+				
+				for (int i = tamanhoRealArray - 1; i >= index; i--) {
+					String ant = elementos[i];
+					elementos[i + 1] = ant;
+					
+				}
+				elementos[index] = elemento;
+				this.tamanhoRealArray++;
+				return true;
+				
+			}else {
+				throw new IllegalArgumentException("Index não existe");
+			}
+		}
 	}
-	
+
+	public boolean adiciona(String elemento) {
+
+		if (tamanhoRealArray < this.elementos.length) {
+			this.elementos[this.tamanhoRealArray] = elemento;
+			this.tamanhoRealArray++;
+			return true;
+		}
+		return false;
+	}
+
 	public int tamanho() {
 		return this.tamanhoRealArray;
 	}
-	
+
 	public String busca(int index) {
-		
-		if(!(index >= 0 && index < tamanhoRealArray)) {
+
+		if (!(index >= 0 && index < tamanhoRealArray)) {
 			throw new IllegalArgumentException("Index não existe");
 		}
-		
+
 		return elementos[index];
 	}
-	
+
 	public int busca(String elementoBusca) {
-		
+
 		for (int i = 0; i < this.tamanhoRealArray; i++) {
 			if (elementoBusca.equals(elementos[i])) {
 				return i;
 			}
 		}
-		
+
 		return -1;
-		
+
 	}
 
 	@Override
 	public String toString() {
-		
-		
+
 		StringBuilder s = new StringBuilder();
 		s.append("[");
-		
+
 		for (int i = 0; i < this.tamanhoRealArray - 1; i++) {
-			
+
 			s.append(elementos[i]);
 			s.append(", ");
 		}
-		
-		if(tamanhoRealArray > 0) {
+
+		if (tamanhoRealArray > 0) {
 			s.append(elementos[this.tamanhoRealArray - 1]);
 		}
-		
+
 		s.append("]");
 		return s.toString();
 	}
-
 
 }
---
+++ src/br/com/talthur/estruturadadosvetor/teste/Aula7.java
@@ -0,0 +1,18 @@
+package br.com.talthur.estruturadadosvetor.teste;
+
+import br.com.talthur.estruturadadosvetor.Vetor;
+
+public class Aula7 {
+	
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		vetor.adiciona("2");
+		vetor.adiciona("4");
+		vetor.adiciona("5");
+		vetor.adiciona("7");
+		
+		vetor.adicionQualquerLugar(0, "2");
+		System.out.println(vetor.toString());
+		}
+
+}
